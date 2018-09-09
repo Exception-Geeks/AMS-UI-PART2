4680cbbbe074f9e214a2873589a9f3da37cbeaac
imnishant <nishantmaharishi@gmail.com>
2018-09-10 00:35:05 +0530

    UI

---
+++ AttendanceManagement/Controllers/ManageAttendanceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttendanceManagement.Models;
+using System.Data.Entity;
+
+namespace AttendanceManagement.Controllers
+{
+    public class ManageAttendanceController : Controller
+    {
+        private AttendanceManagementDBEntities db = new AttendanceManagementDBEntities();
+
+        // GET: ManageAttendance/Index
+        public ActionResult Index()
+        {
+
+            ViewBag.Department_DID = new SelectList(db.Departments, "DID", "Name");
+            ViewBag.Section = new SelectList(db.Students, "USN",  "Section", "Sem" );
+            ViewBag.Sem = new SelectList(db.Students, "USN", "Sem");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int semester, String section)
+        {
+            ViewBag.Department_DID = new SelectList(db.Departments, "DID", "Name");
+            ViewBag.Section = new SelectList(db.Students, "USN", "Section", "Sem");
+            ViewBag.Sem = new SelectList(db.Students, "USN", "Sem");
+
+            var exams = db.Students.Include(s => s.Department_DID).Where(s => s.Sem == semester).Where(s => s.Section.Equals(section));
+            return View(exams.ToList());
+
+        }
+
+
+
+    }
+}
