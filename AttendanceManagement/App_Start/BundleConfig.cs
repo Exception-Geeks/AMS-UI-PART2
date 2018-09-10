350dc2ae592b056613b1c1a80ccc757ee2d3beb7
sankalp7654 <sankalp.saxena.sta@gmail.com>
2018-09-10 11:11:40 +0530

    integrated 3 pages with front end

--- AttendanceManagement/App_Start/BundleConfig.cs
+++ AttendanceManagement/App_Start/BundleConfig.cs
@@ -21,7 +21,13 @@ namespace AttendanceManagement
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap-datetimepicker.js")); //Added for bootstrap date time picker
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/bootstrap-datetimepicker.css")); // Added for bootstra[ date time picker
         }
     }
 }
